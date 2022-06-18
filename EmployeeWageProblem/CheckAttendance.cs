400aa076fe4e145c6e3f568a6254b484295145e2
Siddhant <santoshshrivastava39@gmail.com>
2022-06-19 00:10:26 +0530

    UC5 solution added

--- EmployeeWageProblem/CheckAttendance.cs
+++ EmployeeWageProblem/CheckAttendance.cs
@@ -8,34 +8,41 @@ namespace EmployeeWageProblem
 {
     public class CheckAttendance
     {
-        public void CalculateEmployeeWage()
+        public const int IsPartTime = 2;
+        public const int IsFullTime = 1;
+        public const int IsAbsent = 0;
+        public void CalculateWageForMonth()
         {
 
-            int FullDayHr = 8;
+            int Totalwage = 0;
             int WagePerHr = 20;
-            int totalWage = 0;
             int PartTimeHr = 4;
-            Random random = new Random();
-            int number = random.Next(0, 3);
-            switch (number)
+            int FullTimeHr = 8;
+            int NumberOfDaysPerMonth = 20;
+
+            for (int i = 0; i < NumberOfDaysPerMonth; i++)
             {
-                case 0:
-                    Console.WriteLine("Employee is absent");
-                    break;
-                case 1:
-                    Console.WriteLine("Employee is present");
-                    totalWage = FullDayHr * WagePerHr;
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is present and part time employee");
-                    totalWage = PartTimeHr * WagePerHr;
-                    break;
+                Random random = new Random();
+                int number = random.Next(0, 3);
+                switch (number)
+                {
+                    case IsFullTime:
+                        Console.WriteLine("Employee is Full Time");
+                        Totalwage = Totalwage + WagePerHr * FullTimeHr;
+                        break;
+                    case IsPartTime:
+                        Console.WriteLine("Employee is working part time");
+                        Totalwage = Totalwage + WagePerHr * PartTimeHr;
 
+                        break;
+                    case IsAbsent:
+                        Console.WriteLine("Employee is Absent");
+                        break;
+
+
+                }
+                Console.WriteLine("Employee's Total Wage is" + Totalwage);
             }
-            Console.WriteLine("Employee's total wage  is " + totalWage);
-
-
-
         }
     }
 }
